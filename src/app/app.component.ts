00df4f0f33a37fb9c57e3997e3aff86a15f81fcd
andrzejpuzynski <andrzejpuzynski@tlen.pl>
2020-09-20 21:46:25 +0200

    Add displaying quotes from database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { QUOTES } from './models/database';
+import { Quotation } from './models/quotation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+}
